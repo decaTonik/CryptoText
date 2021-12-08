a2f00d1ec326a0dba02ba79e8ce31a70882b4c71
Nikhil <nikhilsrivastav700@gmail.com>
2021-12-08 18:55:16 +0530

    RSA linked

---
+++ app/src/main/java/com/example/cryptotext/RSA.java
@@ -0,0 +1,14 @@
+package com.example.cryptotext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RSA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_r_s_a);
+    }
+}
--- app/src/main/java/com/example/cryptotext/SetupActivity.java
+++ app/src/main/java/com/example/cryptotext/SetupActivity.java
@@ -1,14 +1,53 @@
 package com.example.cryptotext;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SetupActivity extends AppCompatActivity {
 
+    ImageButton logout, rsa, aes;
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    String profileImageUrl, profileUsername;
+    CircleImageView profileImageHeader;
+    TextView usernameHeader;
+
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
+        rsa = findViewById(R.id.rsa);
+        aes = findViewById(R.id.aes);
+
+        toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Crypto Text");
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        rsa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), RSA.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
