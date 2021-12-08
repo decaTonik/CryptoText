3fd06cf35427dd384cc4546b8f1533e8d45e2ff2
Nikhil <nikhilsrivastav700@gmail.com>
2021-12-08 22:26:56 +0530

    Saved data to Realtime Database

--- app/src/main/java/com/example/cryptotext/RSA.java
+++ app/src/main/java/com/example/cryptotext/RSA.java
@@ -3,12 +3,16 @@ package com.example.cryptotext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.cryptotext.SetupActivity;
 
-public class RSA extends AppCompatActivity {
+public class RSA extends SetupActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_r_s_a);
+
+
+
     }
 }
