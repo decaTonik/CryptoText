4c4b4326f2848ff81b6e1bdcd0c327dd42defde2
Nikhil <nikhilsrivastav700@gmail.com>
2021-12-09 20:12:52 +0530

    Send Friend Request Completed

--- app/src/main/java/com/example/cryptotext/ViewFriendActivity.java
+++ app/src/main/java/com/example/cryptotext/ViewFriendActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,33 +20,128 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ViewFriendActivity extends AppCompatActivity {
 
-    DatabaseReference mUserRef;
+    DatabaseReference mUserRef, requestRef, friendRef;
     FirebaseAuth mAuth;
     FirebaseUser mUser;
 
     CircleImageView profileImage;
     TextView name;
-
     String ImageUrl, username;
+    Button btnPerform, btnDecline;
+    String currentState = "nothing_happen";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String userID = getIntent().getStringExtra("userKey");
+        final String userID = getIntent().getStringExtra("userKey");
         setContentView(R.layout.activity_view_friend);
 
         mUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+        requestRef = FirebaseDatabase.getInstance().getReference().child("Requests");
+        friendRef = FirebaseDatabase.getInstance().getReference().child("Friends");
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
         profileImage = findViewById(R.id.profileImageF);
         name = findViewById(R.id.usernameF);
+        btnPerform = findViewById(R.id.btnPerform);
+        btnDecline = findViewById(R.id.btnDecline);
+        btnDecline.setVisibility(View.GONE);
 
         LoadUser();
 
+        btnPerform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performAction(userID);
+            }
+        });
+
+    }
+
+    private void performAction(String userID) {
+        if(currentState.equals("nothing_happen"))
+        {
+            HashMap hashMap = new HashMap();
+            hashMap.put("status", "pending");
+            requestRef.child(mUser.getUid()).child(userID).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(ViewFriendActivity.this, "Friend Request Sent!", Toast.LENGTH_SHORT).show();
+                        btnDecline.setVisibility(View.GONE);
+                        currentState = "I_sent_pending";
+                        btnPerform.setText("Cancel Friend Request");
+                    }
+                    else
+                    {
+                        Toast.makeText(ViewFriendActivity.this, ""+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+        }
+        if(currentState.equals("I_sent_pending") || currentState.equals("I_sent_decline"))
+        {
+            requestRef.child(mUser.getUid()).child(userID).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(ViewFriendActivity.this, "Friend Request Cancelled", Toast.LENGTH_SHORT).show();
+                        btnPerform.setText("Send Friend Request");
+                        btnDecline.setVisibility(View.GONE);
+                        currentState = "nothing_happen";
+                    }
+                    else
+                    {
+                        Toast.makeText(ViewFriendActivity.this, ""+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        if(currentState.equals("he_sent_pending"))
+        {
+            requestRef.child(mUser.getUid()).child(userID).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful())
+                    {
+                        HashMap hashMap = new HashMap();
+                        hashMap.put("status", "friend");
+                        hashMap.put("username", username);
+                        hashMap.put("ImageUrl", ImageUrl);
+                        friendRef.child(mUser.getUid()).child(userID).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                            @Override
+                            public void onComplete(@NonNull Task task) {
+                                if(task.isSuccessful())
+                                {
+                                    friendRef.child(userID).child(mUser.getUid()).updateChildren(hashMap).addOnCompleteListener(new OnCompleteListener() {
+                                        @Override
+                                        public void onComplete(@NonNull Task task) {
+                                            Toast.makeText(ViewFriendActivity.this, "Friend Added", Toast.LENGTH_SHORT).show();
+                                            currentState = "friends";
+                                            btnPerform.setClickable(false);
+                                            btnPerform.setText("Friends");
+                                            btnDecline.setText("Unfriend");
+                                            btnDecline.setVisibility(View.VISIBLE);
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 
     private void LoadUser() {
